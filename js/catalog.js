a62137faf61b2831535ee164643dfe4703a86f5c
Yaroslav Ulianitskyi <yaroslav.ulk@gmail.com>
2024-02-25 22:57:47 +0200

    added js

---
+++ js/catalog.js
@@ -0,0 +1,146 @@
+// button show more products *****
+
+const mainCatalogContainer = document.querySelector(".main_container_catalog");
+
+const btnShowMore = document.querySelector(".show_more"); //btn show more products
+const catalog = document.querySelector(".catalog"); // catalog wrapper
+
+function showMoreProducts(event) {
+  if (event.target === btnShowMore) {
+    //by the date attribute, we determine the product that is already on the page
+    const productsOnPage = document.querySelectorAll(".detail"); //get node list
+    let inPage = [];
+    productsOnPage.forEach((elem) => {
+      inPage.push(elem.dataset.id);
+    });
+    //determine by ID products that are not yet on the page
+    let inCtalog = [];
+    productsCatalog.map((elem) => {
+      if (!inPage.includes(elem.id)) inCtalog.push(elem);
+    });
+
+    let outProductInPage = "";
+
+    if (inCtalog.length < 1) {
+      btnShowMore.style.display = "none";
+      return;
+    } else {
+      for (let i = 0; i < 3; i++) {
+        outProductInPage += `<li class="item_card">
+        <div class="item_img">
+          <img src="${inCtalog[i].image}" alt="#" />
+        </div>
+        <h3>${inCtalog[i].name}</h3>
+        <p class="item_price">${inCtalog[i].price + " грн"}</p>
+        <div class="item_btns">
+          <span class="detail" data-id="${inCtalog[i].id}">Детальніше</span>
+          <div class="in_basket_wrap" >
+            <button class="in_basket" data-index="${
+              inCtalog[i].id
+            }">В кошик</button>
+          </div>
+        </div>
+      </li>`;
+      }
+    }
+    catalog.insertAdjacentHTML("beforeend", outProductInPage);
+  }
+}
+
+mainCatalogContainer.addEventListener("click", showMoreProducts);
+
+// END button show more products *****
+
+//creation and display of the product page *****
+
+const backArrow = document.querySelector(".return"); // btn back
+const productContainer = document.querySelector(".container_product_card"); //container with products information
+const catalogContainer = document.querySelector(".container_catalog"); // container with products
+const productsInfo = document.querySelectorAll(".detail"); //btn to show products page
+
+function showProductPage(event) {
+  if (event.target.dataset.id) {
+    //all variables for editing product information
+    const productImg = document.querySelector(".information_img");
+    const productTitle = document.querySelector(".information_title");
+    const productDisc = document.querySelector(".information_discr");
+    const productPrice = document.querySelector(".price");
+    const productCompos = document.querySelector(".product_composition");
+    const dtnProductInBasket = document.querySelector(".product_to_basket");
+
+    productsCatalog.forEach((elem) => {
+      if (elem.id === event.target.dataset.id) {
+        productImg.setAttribute("src", elem.image);
+        productTitle.innerText = elem.name;
+        productDisc.innerText = elem.discription;
+        productPrice.innerText = elem.price + " грн";
+        productCompos.innerText = elem.composition;
+        dtnProductInBasket.setAttribute("data-index", elem.id);
+      }
+    });
+
+    productContainer.classList.toggle("active");
+    catalogContainer.classList.toggle("active");
+
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  }
+
+  // btn back
+  backArrow.onclick = () => {
+    productContainer.classList.toggle("active");
+    catalogContainer.classList.toggle("active");
+  };
+}
+mainCatalogContainer.addEventListener("click", showProductPage);
+
+// ENDcreation and display of the product page *****
+
+
+
+// operations with basket ****
+
+//add product to basket
+const btnToBasket = document.querySelector(".in_basket");
+
+function addProductToBasket(event) {
+  if (event.target.dataset.index) {
+    productsCatalog.forEach((elem) => {
+      if (elem.id === event.target.dataset.index) {
+        event.target.innerHTML = "&#10003;";
+        if(basket.includes(elem)) {
+          elem.counter++;
+        } else {
+        basket.push(elem);
+        }
+      }
+      productInBasket.innerText = basket.length;
+    });
+  }
+  sessionStorage.setItem("basket", JSON.stringify(basket));
+}
+
+mainCatalogContainer.addEventListener("click", addProductToBasket);
+
+// remove product from basket
+
+
+function removeFromBasket (event) {
+  if (event.target.dataset.index) {
+    let index;
+    for (let i = 0; i < basket.length; i++) {
+        if (basket[i].id === event.target.dataset.index) {
+            index = i; 
+        }
+}
+basket.splice(index, 1);
+
+sessionStorage.setItem("basket", JSON.stringify(basket));
+productInBasket.innerText = basket.length;
+  }
+ 
+}
+
+
+basketWindow.addEventListener("click", removeFromBasket);
+
+//// remove product from basket
--- js/main.js
+++ js/main.js
@@ -1,30 +1,75 @@
-// berger menu
-const headerBurger = document.querySelector(".menu_burger");
-const menuBtns = document.querySelectorAll(".burger_line");
-const navMenu = document.querySelector(".navigate");
+const productsCatalog = JSON.parse(localStorage.getItem("goods")); //receive a catalog of goods from local storage
+
+// berger menu ******
+
+const headerBurger = document.querySelector(".menu_burger"); //burger-menu btn for click
+const menuBtns = document.querySelectorAll(".burger_line"); //burger-menu line for animating
+const navMenu = document.querySelector(".navigate"); // navigate
+
 headerBurger.onclick = () => {
   menuBtns.forEach((btn) => {
     btn.classList.toggle("opened");
   });
   navMenu.classList.toggle("opened_nav");
 };
-// END berger menu
 
+// END berger menu *****
 
+//Show basket ***
 
-//tabs for basket
-const showBasket = document.querySelector(".basket_icon");
-const basket = document.querySelector(".show_basket");
-const continueBuy = document.querySelector(".continue");
+const showBasket = document.querySelector(".basket_icon"); //basket btn for click
+const basketWindow = document.querySelector(".show_basket"); //displaying the shopping cart window
+const continueBuy = document.querySelector(".continue"); // button close basket
 const header = document.querySelector("header");
-const showOrder = document.querySelector(".to_order");
-const toOrder = document.querySelector(".buy");
+const showOrder = document.querySelector(".to_order"); //window form for ordering
+const toOrder = document.querySelector(".buy"); //btn for show ordring window
+const basketContainer = document.querySelector(".basket_items");
+const quanOfProducts = document.querySelector(".quantity_of_products");
+const totalSum = document.querySelector(".total_sum");
+const productInBasket = document.querySelector(".product_in_busket");
+
+let basket = [];
+
+function checkLocalBasket() {
+  if (sessionStorage.getItem("basket") != null) {
+    basket = JSON.parse(sessionStorage.getItem("basket"));
+  }
+}
+
+checkLocalBasket();
+
+productInBasket.innerText = basket.length;
+
 function showMyBasket(event) {
+  let outProductInBasket = "";
+  for (let i = 0; i < basket.length; i++) {
+    outProductInBasket += `<div class="basket_item">
+      <div class="basket_items_img">
+        <img src="${basket[i].image}" alt="#" />
+      </div>
+      <div class="basket_item_info">
+        <div class="item_info">
+          <h3>${basket[i].name}</h3>
+          <span data-index="${basket[i].id}" class="delete"></span>
+        </div>
+          <p>${basket[i].price} грн</p>
+      </div>
+    </div>`;
+  }
+  totalSum.innerText = basket.reduce((sum, elem) => {
+    return sum + elem.price;
+  }, 0);
+
+  quanOfProducts.innerText = basket.length;
+  basketContainer.innerHTML = outProductInBasket;
+
   if (event.target === showBasket || event.target === continueBuy) {
-    basket.classList.toggle("show_basket_active");
+    basketWindow.classList.toggle("show_basket_active");
   } else if (event.target === toOrder) {
     showOrder.classList.toggle("to_order_active");
   }
 }
+
 header.addEventListener("click", showMyBasket);
-// END tabs for basket
+
+//Show basket ***
