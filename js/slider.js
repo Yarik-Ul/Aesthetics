855b087f777e03e737c168d0f075f9ce5501d759
Yaroslav Ulianitskyi <yaroslav.ulk@gmail.com>
2024-03-01 00:39:51 +0200

    added slider dots

--- js/slider.js
+++ js/slider.js
@@ -1,9 +1,26 @@
 //Slider
 const sliderContainer = document.querySelector(".slider_wrap");
-const sliderlItems = document.querySelectorAll(".slider_item");
-const elWidth = sliderlItems[0].offsetWidth;
+const sliderItems = document.querySelectorAll(".slider_item");
+const sliderDot = document.querySelectorAll(".dot");
+const elWidth = sliderItems[0].offsetWidth;
+let sliderInterval = 2500;
+let sliderCounter = 0;
 
 function moveSlider() {
+  //pagination
+  sliderCounter++;
+  if (sliderCounter === sliderItems.length) {
+    sliderCounter = 0;
+  }
+
+  sliderDot.forEach((elem) => {
+    if (Number(elem.dataset.dot) === sliderCounter) {
+      elem.style.background = "#4b6049";
+    } else {
+      elem.style.background = "none";
+    }
+  });
+  //slide switch
   sliderContainer.classList.add("slider_animating");
   sliderContainer.style.transform = `translateX(-${elWidth}px)`;
 
@@ -14,5 +31,5 @@ function moveSlider() {
   }, 600);
 }
 
-setInterval(moveSlider, 2000);
+const intervalId = setInterval(moveSlider, sliderInterval);
 //END Slider
